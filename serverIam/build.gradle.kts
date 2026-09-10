aa034fdadd8f84f593e4e91f4b2889c8002fad6c
virang <fastandfurious.viru@gmail.com>
2026-09-10 15:12:16 +0200

    add barebones setup files for IAM server

---
+++ serverIam/build.gradle.kts
@@ -0,0 +1,30 @@
+plugins {
+    java
+    id("org.springframework.boot") version "4.1.1"
+    id("io.spring.dependency-management") version "1.1.7"
+}
+
+group = "org.example"
+version = "0.0.1-SNAPSHOT"
+description = "serverIam"
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(24)
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
---
+++ serverIam/src/main/java/org/example/serveriam/ServerIamApplication.java
@@ -0,0 +1,13 @@
+package org.example.serveriam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerIamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServerIamApplication.class, args);
+    }
+
+}
